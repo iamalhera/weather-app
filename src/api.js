72b416d8750c2dc7c74c06d0fd2a9fd1635fbc84
Al Hera Ahmad <alheraahmad0786@gmail.com>
2022-08-26 00:45:08 +0530

    API's Added

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '954541a05cmsh8bb18e1c4827f17p18287ajsnb790d5d3a4bd',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const CURRENT_WEATHER_URL =  "https://api.openweathermap.org/data/2.5" ;
+export const WEATHER_API_KEY = "d9ef0cc296f23702e5fdf8ccbbf6c442" ;
+
